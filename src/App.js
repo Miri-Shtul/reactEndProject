0b6aff818c767e79df484bd75d4a39157ee098c1
github_username <email_address>
2023-03-14 23:23:12 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import Login from './components/Login/Login';
+import UserContext from './components/UserContext';
+import MyRoute from './routes/MyRoute';
 
 function App() {
+  const [isLogin, setIsLogin] = useState(false)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <UserContext>
+      {!isLogin ?
+        <Login setIsLogin={setIsLogin} /> :
+        <MyRoute setIsLogin={setIsLogin}/>
+      }
+    </UserContext>
   );
 }
 
